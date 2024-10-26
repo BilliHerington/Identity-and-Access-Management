cd786399d00b1c61a73a9e51c67f7e5a8853d974
Denis <kukuruza774@gmail.com>
2024-10-26 09:46:23 +0300

    bug fix

--- go.mod
+++ go.mod
@@ -9,8 +9,12 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/crypto v0.27.0
 	golang.org/x/oauth2 v0.23.0
+	google.golang.org/api v0.197.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
 require (
@@ -22,9 +26,8 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43 // indirect
-	github.com/emersion/go-smtp v0.21.3 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -46,7 +49,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.opencensus.io v0.24.0 // indirect
@@ -58,12 +62,8 @@ require (
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
-	google.golang.org/api v0.197.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/grpc v1.66.1 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/tests/userTests/deleteUser_test.go
+++ pkg/tests/userTests/deleteUser_test.go
@@ -1,8 +1,15 @@
-package rolesTest
+package userTests
 
 import (
 	"IAM/pkg/handlers/users"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -36,5 +43,85 @@ func TestDeleteUser_Success(t *testing.T) {
 	users.UserManageRepo = mockRepo
 
 	// create HTTP-request
-	
+	body := map[string]string{"email": "test@example.com"}
+	jsonBody, _ := json.Marshal(body)
+	req, _ := http.NewRequest("POST", "/deleteUser", bytes.NewBuffer(jsonBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	// use httptest for recording answer
+	w := httptest.NewRecorder()
+
+	// create Gin-context and call handler
+	c, _ := gin.CreateTestContext(w)
+	c.Request = req
+
+	// call func DeleteUser
+	handler := users.DeleteUser()
+	handler(c)
+
+	// check result
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, `{"user deleted":"test@example.com"}`, w.Body.String())
+	mockRepo.AssertExpectations(t)
+}
+
+func TestDeleteUser_UserDoesNotExist(t *testing.T) {
+	// mocks setting
+	mockRepo := new(MockUserManagementRepository)
+	mockRepo.On("DeleteUserFromDB", "nonexistent@example.com").Return(errors.New("user does not exist"))
+
+	// set new Global repo var
+	users.UserManageRepo = mockRepo
+
+	// create HTTP-request
+	body := map[string]string{"email": "nonexistent@example.com"}
+	jsonBody, _ := json.Marshal(body)
+	req, _ := http.NewRequest("POST", "/delete-user", bytes.NewBuffer(jsonBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	// use httptest for recording answer
+	w := httptest.NewRecorder()
+
+	// create Gin-context and call handler
+	c, _ := gin.CreateTestContext(w)
+	c.Request = req
+
+	// call func DeleteUser
+	handler := users.DeleteUser()
+	handler(c)
+
+	// check result
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.JSONEq(t, `{"error": "user does not exist"}`, w.Body.String())
+	mockRepo.AssertExpectations(t)
+}
+func TestDeleteUser_InvalidEmail(t *testing.T) {
+	//mockRepo := new(MockUserManagementRepository)
+	//
+	//// Здесь не надо настраивать mockRepo, потому что оно не должно быть вызвано при ошибке валидации
+	//users.UserManageRepo = mockRepo
+
+	// Некорректный email
+	body := map[string]string{"email": "emailWithoutMailTag.com"}
+	jsonBody, _ := json.Marshal(body)
+	req, _ := http.NewRequest("POST", "/delete-user", bytes.NewBuffer(jsonBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	// Записываем ответ
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Request = req
+
+	// Вызываем обработчик
+	handler := users.DeleteUser()
+	handler(c)
+
+	// Ожидаем, что код ответа будет 400
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	// Проверяем тело ответа
+	assert.JSONEq(t, `{"error": "incorrect data format, please check your input data"}`, w.Body.String())
+
+	// Проверяем, что метод DeleteUserFromDB не был вызван
+	//mockRepo.AssertNotCalled(t, "DeleteUserFromDB", "emailWithoutMailTag.com")
 }
